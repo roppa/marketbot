73e12b63e645fcf228ad8d9b4f524e774ce5000d
Mark Robson <mark.o.robson@gmail.com>
2020-09-09 15:28:30 +0100

    optimisation tweaks

--- bot/__tests__/bot.spec.js
+++ bot/__tests__/bot.spec.js
@@ -1,21 +1,18 @@
 const axios = require('axios')
 const bot = require('..')
-jest.useFakeTimers();
+jest.useFakeTimers()
 
 jest.mock('axios')
 
-const response = module.exports = [
-  [302, 1, 20
-  ], [
-    168.02, 1, 0.6
-  ], [
-    167.98, 1, 0.6
-  ], [
-    167.94, 1, 0.6
-  ]]
+const response = (module.exports = [
+  [302, 1, 20],
+  [168.02, 1, 0.6],
+  [167.98, 1, 0.6],
+  [167.94, 1, 0.6],
+])
 
 beforeEach(() => {
-  jest.spyOn(console, 'log').mockImplementation(() => { });
+  jest.spyOn(console, 'log').mockImplementation(() => {})
 })
 
 describe('bot balance', () => {
@@ -27,41 +24,57 @@ describe('bot balance', () => {
 
 describe('bot orderbook', () => {
   test('should retrieve eth-usd order', async () => {
-    axios.get.mockImplementationOnce(async () => ({ data: response }))
+    axios.get.mockImplementationOnce(async () => ({
+      data: response,
+    }))
     const order = await bot.order()
     expect(order).toEqual(response)
   })
 })
 
 describe('bot bid', () => {
-  test('should bid 5 orders within 5% of bid price', async () => {
-    axios.get.mockImplementationOnce(async () => ({ data: response }))
-    const bids = await bot.bid()
+  test('should bid 5 orders within 5% of bid price', () => {
+    axios.get.mockImplementationOnce(() => ({
+      data: response,
+    }))
+    const bids = bot.bid(response)
     expect(bids.length).toEqual(response.length)
     expect(bids[0].length).toEqual(5)
-    expect(bids[0][0] <= response[0][0] + response[0][0] * .05).toBeTruthy()
+    expect(bids[0][0] <= response[0][0] + response[0][0] * 0.05).toBeTruthy()
   })
 })
 
 describe('bot ask', () => {
-  test('should ask 5 orders within 5% of ask price', async () => {
-    axios.get.mockImplementationOnce(async () => ({ data: response }))
-    const asks = await bot.ask()
+  test('should ask 5 orders within 5% of ask price', () => {
+    axios.get.mockImplementationOnce(async () => ({
+      data: response,
+    }))
+    const asks = bot.ask(response)
     expect(asks.length).toEqual(response.length)
     expect(asks[0].length).toEqual(5)
-    expect(asks[0][0] >= response[0][0] - response[0][0] * .05).toBeTruthy()
+    expect(asks[0][0] >= response[0][0] - response[0][0] * 0.05).toBeTruthy()
   })
 })
 
-describe('bot listen', () => {
+describe('logOrders', () => {
+  test('should log orders to console', () => {
+    const log = jest.spyOn(console, 'log')
+    bot.logOrders('PLACE BID', response)
+    expect(log).toHaveBeenCalled()
+  })
+})
+
+describe('bot run', () => {
   test('should loop', () => {
-    axios.get.mockImplementation(async () => ({ data: response }))
+    axios.get.mockImplementation(async () => ({
+      data: response,
+    }))
     const run = jest.spyOn(bot, 'run')
-    const bid = jest.spyOn(bot, 'bid')
+    const log = jest.spyOn(console, 'log')
     bot.start()
-    expect(setInterval).toHaveBeenCalled();
-    jest.runOnlyPendingTimers();
-    expect(run).toHaveBeenCalledTimes(2);
-    expect(bid).toHaveBeenCalled();
+    expect(setInterval).toHaveBeenCalled()
+    jest.runOnlyPendingTimers()
+    expect(run).toHaveBeenCalledTimes(2)
+    expect(log).toHaveBeenCalled()
   })
 })
--- bot/index.js
+++ bot/index.js
@@ -1,58 +1,64 @@
 const axios = require('axios')
 const params = {
-  Symbol: "tETHUSD",
-  Precision: "P0",
-};
+  Symbol: 'tETHUSD',
+  Precision: 'P0',
+}
 
-const order = () => axios.get(`https://api.stg.deversifi.com/bfx/v2/book/${params.Symbol}/${params.Precision}`, {
-  ...params,
-  headers: {
-    "Accept": "application/json",
-    "Content-Type": "application/json",
-  }
-}).then(response => response.data)
+const order = () =>
+  axios
+  .get(
+    `https://api.stg.deversifi.com/bfx/v2/book/${params.Symbol}/${params.Precision}`, {
+      ...params,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+  .then((response) => response.data)
 
-const getPercent = (percent) => Math.floor(Math.random() * percent) / 100
+const getRandomPercent = (percent) => Math.floor(Math.random() * percent) / 100
 
-const getPercentOfOrders = (percent, orders, operation) => orders.map(order => {
-  const result = []
-  for (let i = 0; i < 5; i++) {
-    result.push(operation(order[0], (order[0] * getPercent(5))))
-  }
-  return result
-})
+const operatePercentOnOrders = (operation, percent, orders) =>
+  orders.map((order) => {
+    const result = []
+    for (let i = 0; i < 5; i++) {
+      result.push(operation(order[0], order[0] * getRandomPercent(percent)))
+    }
+    return result
+  })
 
 const add = (a, b) => a + b
 const minus = (a, b) => a - b
 
-const bid = async () => getPercentOfOrders(5, await order(), add)
+const bid = (orders) => operatePercentOnOrders(add, 5, orders)
+const ask = (orders) => operatePercentOnOrders(minus, 5, orders)
 
-const ask = async () => getPercentOfOrders(5, await order(), minus)
-
-const log = (orderType, amount) => console.log(`${orderType} @ ${amount}`)
-
-const logOrders = (orderType, orders) => orders.map(order => log(orderType, order))
+const logOrders = (orderType, orders) =>
+  orders.map((amount) => console.log(`${orderType} @ ${amount}`))
 
 const bot = {
   balance: {
     eth: 10,
-    usd: 2000
+    usd: 2000,
   },
+  logOrders,
   order,
   bid,
   ask,
   run,
-  start
+  start,
 }
 
 async function run() {
-  const bids = await bot.bid()
-  const asks = await bot.ask()
+  const orders = await order()
+  const bids = bot.bid(orders)
+  const asks = bot.ask(orders)
   logOrders('PLACE BID', bids)
   logOrders('FILLED ASK', asks)
 }
 
-async function start() {
+function start() {
   bot.run()
   const loop = setInterval(() => {
     bot.run()
